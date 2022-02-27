758c3d882d00094db01a94862a40b16bcb6650d6
Mojol12 <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@github.com>
2022-02-27 20:18:41 +0200

    yout commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<head>
+<style>
+
+.body {
+background-color:  #cd3456;
+
+}
+
+.p{
+color: #cd7856;
+}
+</style>
+<title>my project</title>
+
+</head>
+<body>
+<p>my project is very cool </p>
+
+</body></html>
+
+
